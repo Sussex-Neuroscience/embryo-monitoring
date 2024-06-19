fdeeef86167ffdbe35b814acaaf851d8e828f785
amchagas <andremaia.chagas@gmail.com>
2024-06-19 17:44:04 +0100

    more steps

--- software/python_new/test.py
+++ software/python_new/test.py
@@ -15,12 +15,17 @@ data_file = "converted.mp4"
 #the main code loop... this variable just below this needs to be set to False if one is running the actual experiments
 pause_between_frames = True
 
+cv.namedWindow('slide')
+cv.createTrackbar('threshold','slide',0,255,nothing)
+cv.setTrackbarPos('threshold','slide',80)
 
 cap = cv.VideoCapture(data_folder+data_file)
 #cap = cv.VideoCapture(0)
 if not cap.isOpened():
- print("Cannot open camera")
- exit()
+    print("Cannot open camera")
+    exit()
+
+valid,raw_image = cap.read()
 
 frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
@@ -30,20 +35,32 @@ fps = int(cap.get(cv.CAP_PROP_FPS))
 
 #create named windows to show the raw and detected data
 #cv.namedWindow('binary')
-cv.namedWindow('slide',cv.WINDOW_GUI_NORMAL)
+
+cv.resizeWindow(winname="slide", width=frame_width+5, height=frame_height+5	)
 #cv.namedWindow('original image', cv.WINDOW_NORMAL)
 #create a trackbar to change the threshold for each movie
-cv.createTrackbar('threshold','slide',0,255,nothing)
+
+
+roi = cv.selectROI("frame", raw_image, showCrosshair=True) 
+cv.imshow("slide",raw_image)
+cv.waitKey(0)
+#thresh_value=100
 while True:
     # get current positions of the trackbars
     thresh_value = cv.getTrackbarPos('threshold','slide')
     #grab one frame:
     valid,raw_image = cap.read()
+    #gray = cv.cvtColor(raw_image, cv.COLOR_BGR2GRAY)
     ret,binary_image = cv.threshold(raw_image,thresh_value,255,cv.THRESH_BINARY)
 
     # Display the resulting frame
-    cv.imshow(winname="slide",mat= binary_image)
+    cv.imshow(winname="slide",mat=binary_image)
+    cv.waitKey(0)
     #cv.imshow(winname="slide")
-    time.sleep(0.1)
-
+    
+    if pause_between_frames:
+        #time.sleep(1/fps)
+        time.sleep(0.1)
 cap.release()
+cv.destroyAllWindows()
+
